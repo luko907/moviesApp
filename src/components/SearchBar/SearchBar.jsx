76c6f797a05059ffd0f40c8a3d1f4ce2e20564a3
Lucas Koch <luko907@hotmail.com>
2021-10-19 19:56:46 -0300

    change logo size

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import styles from "./SearchBar.module.css";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
 
 export default function SearchBar() {
   return (
     <div>
       <form>
         <div className={styles.divContainer}>
-          <div className={styles.icon}>
-            <FontAwesomeIcon icon={faMagnifyingGlass} />
-          </div>
+          <Link to="/search">
+            <div className={styles.icon}>
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </div>
+          </Link>
+
           <input
             type="text"
             className={styles.input_search}
