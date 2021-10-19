f46aec7a5d1f16144af8f191f04e54fea257276d
Lucas Koch <luko907@hotmail.com>
2021-10-19 18:47:21 -0300

    update styles search input

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import styles from "./SearchBar.module.css";
 
 export default function SearchBar() {
   return (
     <div>
       <form>
-        <input type="text" placeholder="Encuentra películas, series y más" />
+        <input
+          type="text"
+          className={styles.input_search}
+          placeholder="Encuentra películas, series y más"
+        />
       </form>
     </div>
   );
