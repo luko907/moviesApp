c43c910901fd799aa34e8c04a93ed1e1d355bb2d
Lucas Koch <luko907@hotmail.com>
2021-10-19 17:37:47 -0300

    styling navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import styles from "./NavBar.module.css";
 import { NavLink } from "react-router-dom";
-import Logo from "../img/logo.svg";
+import Logo from "../../img/logo.svg";
 
 export default function NavBar() {
   return (
-    <header>
-      <div>
+    <header className={styles.header}>
+      <div className={styles.linkContainer}>
         <NavLink exact to="/">
           <img id="" src={Logo} alt="" />
         </NavLink>
