f9f8a96917883da98f86f91e5a3922568fa83587
Lucas Koch <luko907@hotmail.com>
2021-10-19 19:44:36 -0300

    add icon magnifyer and update searchbar styles

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import styles from "./SearchBar.module.css";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function SearchBar() {
   return (
     <div>
       <form>
-        <input
-          type="text"
-          className={styles.input_search}
-          placeholder="Encuentra películas, series y más"
-        />
+        <div className={styles.divContainer}>
+          <div className={styles.icon}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </div>
+          <input
+            type="text"
+            className={styles.input_search}
+            placeholder="Encuentra películas, series y más"
+          />
+        </div>
       </form>
     </div>
   );
