269c7e30d2c9e53d4081b73cb6e6031d514f9ec1
Lucas Koch <luko907@hotmail.com>
2021-10-21 15:10:02 -0300

    create home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import NavBar from "./components/NavBar/NavBar.jsx";
+import Home from "./components/Home/Home.jsx";
 
 function App() {
   return (
     <React.Fragment>
       <NavBar />
+      <div>
+        <Home />
+      </div>
     </React.Fragment>
   );
 }
