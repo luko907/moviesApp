aab965bb8a227116d6dce94ced51fcc582c1e7a1
Lucas Koch <luko907@hotmail.com>
2021-10-21 15:46:17 -0300

    update home styles

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import styles from "./Home.module.css";
 
 function Home() {
   return (
     <React.Fragment>
-      <div>
+      <div className={styles.div_suggestion}>
         <span>Suggestion</span>
-        <ul>
+      </div>
+      <div className={styles.div_hot_top}>
+        <ul className={styles.ul_hot_top}>
           <li>Hot</li>
           <li>Top views</li>
           <li>Top rating</li>
