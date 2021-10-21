6f062547b753ed829c159f55fdd336b65054abde
Lucas Koch <luko907@hotmail.com>
2021-10-21 15:10:02 -0300

    create home component

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Home() {
+  return (
+    <React.Fragment>
+      <div>
+        <span>Suggestion</span>
+        <ul>
+          <li>Hot</li>
+          <li>Top views</li>
+          <li>Top rating</li>
+        </ul>
+      </div>
+    </React.Fragment>
+  );
+}
+export default Home;
