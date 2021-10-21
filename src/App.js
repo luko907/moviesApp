2e95231031672a5a3d594685cd6cf01c5863374e
Lucas Koch <luko907@hotmail.com>
2021-10-21 15:46:17 -0300

    update home styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import NavBar from "./components/NavBar/NavBar.jsx";
 import Home from "./components/Home/Home.jsx";
+import "./App.css";
 
 function App() {
   return (
     <React.Fragment>
       <NavBar />
-      <div>
+      <div className="home">
         <Home />
       </div>
     </React.Fragment>
