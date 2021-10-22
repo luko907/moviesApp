ff963c73223fdba68efa48fd07e4c9a3bfdef4ee
Lucas Koch <luko907@hotmail.com>
2021-10-22 16:08:51 -0300

    add movies route and home

---
+++ src/components/Movies/Movies.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "./Movies.module.css";
+
+function Movies() {
+  return (
+    <React.Fragment>
+      <div className={styles.container}>
+        <div className={styles.div_suggestion}>
+          <span>Suggestion</span>
+        </div>
+        <div className={styles.div_hot_top}>
+          <ul className={styles.ul_hot_top}>
+            <li>
+              <a href="#">Hot</a>
+            </li>
+            <li>
+              <a href="#">Top views</a>
+            </li>
+            <li>
+              <a href="#">Top rating</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
+export default Movies;
