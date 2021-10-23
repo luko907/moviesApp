2f7d7dea956ce3b1fecf56f0c6f56366d88d045f
Lucas Koch <luko907@hotmail.com>
2021-10-23 00:50:56 -0300

    add redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,23 +1,11 @@
 const initialState = {
-  visibilityFilter: "SHOW_ALL",
-  todos: [],
+  moviesLoaded: [],
 };
 
 function appReducer(state = initialState, action) {
   switch (action.type) {
-    case "SET_VISIBILITY_FILTER": {
-      return Object.assign({}, state, {
-        visibilityFilter: action.filter,
-      });
-    }
-    case "ADD_TODO": {
-      return Object.assign({}, state, {
-        todos: state.todos.concat({
-          id: action.id,
-          text: action.text,
-          completed: false,
-        }),
-      });
+    case "GET_MOVIES": {
+      return { ...state, moviesLoaded: action.payload };
     }
 
     default:
