1e0641f362632afea75c993407ed6807d1688fac
Lucas Koch <luko907@hotmail.com>
2021-10-25 12:50:36 -0300

    change http to https

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,7 +5,7 @@
 export function getActual(title) {
   return function (dispatch) {
     return fetch(
-      `http://www.omdbapi.com/?apikey=2b9c4287&s=${title}&type=movie`
+      `https://www.omdbapi.com/?apikey=2b9c4287&s=${title}&type=movie`
     )
       .then((response) => response.json())
       .then((json) => {
@@ -20,30 +20,30 @@ export function getMovies() {
   return function (dispatch) {
     Promise.all([
       fetch(
-        "http://www.omdbapi.com/?apikey=2b9c4287&s=toy+story&type=movie"
+        "https://www.omdbapi.com/?apikey=2b9c4287&s=toy+story&type=movie"
       ).then((value) => value.json()),
       fetch("http://www.omdbapi.com/?apikey=2b9c4287&s=lego&type=movie").then(
         (value) => value.json()
       ),
       fetch(
-        "http://www.omdbapi.com/?apikey=2b9c4287&s=cry+macho&type=movie"
+        "https://www.omdbapi.com/?apikey=2b9c4287&s=cry+macho&type=movie"
       ).then((value) => value.json()),
       fetch("http://www.omdbapi.com/?apikey=2b9c4287&s=cars&type=movie").then(
         (value) => value.json()
       ),
       fetch(
-        "http://www.omdbapi.com/?apikey=2b9c4287&s=spider+man&type=movie"
+        "https://www.omdbapi.com/?apikey=2b9c4287&s=spider+man&type=movie"
       ).then((value) => value.json()),
       fetch(
-        "http://www.omdbapi.com/?apikey=2b9c4287&s=lion+king&type=movie"
+        "https://www.omdbapi.com/?apikey=2b9c4287&s=lion+king&type=movie"
       ).then((value) => value.json()),
       fetch("http://www.omdbapi.com/?apikey=2b9c4287&s=vivo&type=movie").then(
         (value) => value.json()
       ),
 
-      fetch("http://www.omdbapi.com/?apikey=2b9c4287&s=spirit&type=movie").then(
-        (value) => value.json()
-      ),
+      fetch(
+        "https://www.omdbapi.com/?apikey=2b9c4287&s=spirit&type=movie"
+      ).then((value) => value.json()),
     ])
       .then((value) => {
         dispatch({ type: "GET_MOVIES", payload: value });
