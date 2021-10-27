ab883657e9907c9565a9ea0c06d2c44801749963
Lucas Koch <luko907@hotmail.com>
2021-10-26 22:07:41 -0300

    new styles

--- src/components/MovieInfo/MovieInfo.jsx
+++ src/components/MovieInfo/MovieInfo.jsx
@@ -1,20 +1,19 @@
 import React, { useEffect } from "react";
 import styles from "./MovieInfo.module.css";
 import { connect } from "react-redux";
-import background from "../../img/background.png";
 
 function MovieInfo(props) {
-  useEffect(() => {
-    /*    document.querySelector(".NavBar_header__2ZPPt").style.background =
-      "#3f3f3f"; */
+  /*   useEffect(() => {
+   document.querySelector(".NavBar_header__2ZPPt").style.background =
+      "#3f3f3f"; 
     document.body.style.width = "auto";
     document.body.style.background = "black";
-    /* document.body.style.background =
-      "url('https://image.tmdb.org/t/p/w500/eeijXm3553xvuFbkPFkDG6CLCbQ.jpg') no-repeat center center /cover fixed";  */
+   document.body.style.background =
+      "url('https://image.tmdb.org/t/p/w500/eeijXm3553xvuFbkPFkDG6CLCbQ.jpg') no-repeat center center /cover fixed";  
     return () => {
       document.body.style.background = `url(${background}) no-repeat center center fixed`;
     };
-  }, []);
+  }, []); */
   return (
     <React.Fragment>
       <div className={styles.movieInfo_fullscreen}>
