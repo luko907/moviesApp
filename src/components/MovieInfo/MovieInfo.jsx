bdeb1b62ae1e241e4d6bdf5cf1ed4c8d802c29c1
Lucas Koch <luko907@hotmail.com>
2021-10-30 17:15:13 -0300

    add internal database for popular movies imgs

--- src/components/MovieInfo/MovieInfo.jsx
+++ src/components/MovieInfo/MovieInfo.jsx
@@ -4,24 +4,30 @@ import { StyleRoot } from "radium";
 import styles from "./MovieInfo.module.css";
 import { connect } from "react-redux";
 import { useParams } from "react-router-dom";
+import data from "../../data/popular";
 
 function MovieInfo(props) {
   const baseUrl = "https://image.tmdb.org/t/p/w500";
   const [movieDetails, setMovieDetails] = useState([]);
   const params = useParams();
-  const sample =
-    "https://m.media-amazon.com/images/M/MV5BOTg4MGFlZGQtNjgzOS00YzU5LWEzNTEtYzJhMmQyZmI0MzFjXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_.jpg";
+  const [flag, setFlag] = useState(null);
 
   const movieInfofullscreen = {
     display: "flex",
     justifyContent: "center",
-    background: `linear-gradient(180deg, rgba(6,13,23,0) 40%, rgba(6,13,23,1) 48%),linear-gradient(90deg, rgba(6,13,23,0) 93%, rgba(6,13,23,0.6320903361344538) 98%),linear-gradient(270deg, rgba(6,13,23,0) 93%, rgba(6,13,23,0.6320903361344538) 98%),url(${sample}) no-repeat center center / cover fixed`,
+    background: `linear-gradient(180deg, rgba(6,13,23,0) 40%, rgba(6,13,23,1) 48%),linear-gradient(90deg, rgba(6,13,23,0) 93%, rgba(6,13,23,0.6320903361344538) 98%),linear-gradient(270deg, rgba(6,13,23,0) 93%, rgba(6,13,23,0.6320903361344538) 98%),url(${flag}) no-repeat center center / cover fixed`,
     minHeight: "94vh",
+
     "@media (max-width: 1730px)": {
-      background: `linear-gradient(180deg, rgba(6,13,23,0) 20%, rgba(6,13,23,1) 32em),linear-gradient(90deg, rgba(6,13,23,0) 93%, rgba(6,13,23,0.6320903361344538) 98%),linear-gradient(270deg, rgba(6,13,23,0) 93%, rgba(6,13,23,0.6320903361344538) 98%),url(${sample}) no-repeat center center / cover fixed`,
+      background: `linear-gradient(180deg, rgba(6,13,23,0) 20%, rgba(6,13,23,1) 32em),linear-gradient(90deg, rgba(6,13,23,0) 93%, rgba(6,13,23,0.6320903361344538) 98%),linear-gradient(270deg, rgba(6,13,23,0) 93%, rgba(6,13,23,0.6320903361344538) 98%),url(${flag}) no-repeat center center / cover fixed`,
       minHeight: "84vh",
     },
+    "@media (max-width: 550px)": {
+      background: `linear-gradient(180deg, rgba(6,13,23,0) 30%, rgba(6,13,23,1) 20em),linear-gradient(90deg, rgba(6,13,23,0) 93%, rgba(6,13,23,0.6320903361344538) 98%),linear-gradient(270deg, rgba(6,13,23,0) 93%, rgba(6,13,23,0.6320903361344538) 98%),url(${flag}) no-repeat center center / cover fixed`,
+      minHeight: "87vh",
+    },
   };
+  ///////////////////////
 
   useEffect(() => {
     const url = `process.env.REACT_APP_API_TMDB_BASE_URL/${params.id}?api_key=process.env.REACT_APP_API_TMDB_API_KEY&language=en-US`;
@@ -33,8 +39,24 @@ function MovieInfo(props) {
         console.log("error", error);
       }
     };
+    const findImg = () => {
+      for (let i = 0; i < data.length; i++) {
+        if (data[i].id === movieDetails.id) {
+          setFlag(data[i].backDrop);
+          break;
+        } else {
+          setFlag(baseUrl + movieDetails.poster_path);
+        }
+      }
+    };
     fetchData();
-  }, [params.id]);
+    movieDetails.length !== 0 && findImg();
+  }, [
+    params.id,
+    movieDetails.length,
+    movieDetails.id,
+    movieDetails.poster_path,
+  ]);
 
   return (
     <React.Fragment>
@@ -49,7 +71,6 @@ function MovieInfo(props) {
                 alt=""
               />
             </div>
-
             <div className={styles.description_container}>
               <div className={styles.description}>
                 <span className={styles.title}></span>
