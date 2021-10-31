8621fd276a1c4f87749cfddbf9428475f7816036
Lucas Koch <luko907@hotmail.com>
2021-10-31 00:25:56 -0300

    full responsive and charge imgs to falsy database

--- src/data/popular.js
+++ src/data/popular.js
@@ -11,6 +11,58 @@ const data = [
     backDrop:
       "https://m.media-amazon.com/images/M/MV5BY2I3ZTliZDctYzMxZi00NGNmLTgzNWUtNzI5MTQ0Mzk2ODdiXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_.jpg",
   },
+  {
+    title: "venom carnage",
+    id: 580489,
+    backDrop:
+      "https://m.media-amazon.com/images/M/MV5BYjAzMTI0NWMtMmRlNy00YTc1LWFjMWItMzQ0MjIwNjg0N2JmXkEyXkFqcGdeQXVyNjczOTE0MzM@._V1_.jpg",
+  },
+  {
+    title: "hallowen kills",
+    id: 610253,
+    backDrop:
+      "https://m.media-amazon.com/images/M/MV5BZDYxZmRjNzUtNjViNS00NzM2LTg3MTctOTE2NmQ0ZmM2MjQ5XkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_.jpg",
+  },
+
+  {
+    title: "the vault",
+    id: 630004,
+    backDrop:
+      "https://m.media-amazon.com/images/M/MV5BNDIxODFhZGEtMGY0OC00YWQzLTlhODEtYmU0MjhjZjkyNzlhXkEyXkFqcGdeQXVyNjY1MTg4Mzc@._V1_.jpg",
+  },
+
+  {
+    title: "adams family",
+    id: 639721,
+    backDrop:
+      "https://m.media-amazon.com/images/M/MV5BYWU3ZGVmZTUtY2M5ZS00ZTJkLTgxZDMtYTk5NDZiZTlhOTc0XkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_.jpg",
+  },
+  {
+    title: "venom",
+    id: 335983,
+    backDrop:
+      "https://m.media-amazon.com/images/M/MV5BZWU1MTkwODQtMjYyOC00Y2I1LWFlOTgtYmY3MWY2MjBlYTAxXkEyXkFqcGdeQXVyMTYzMDM0NTU@._V1_.jpg",
+  },
+  {
+    title: "zone 414",
+    id: 675319,
+    backDrop:
+      "https://m.media-amazon.com/images/M/MV5BZjFlYjVjOGMtNmM0MC00YmMyLWE3Y2ItNDZkYzUzOWQ2MmQ1XkEyXkFqcGdeQXVyMjQ4OTQwNDU@._V1_.jpg",
+  },
+
+  {
+    title: "the last warrior",
+    id: 566525,
+    backDrop:
+      "https://m.media-amazon.com/images/M/MV5BYzM0YjJlMWEtZTg0MS00NjM4LWJkMDktN2VmZGU2ZjQ1YjI2XkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_.jpg",
+  },
+
+  {
+    title: "pokemon-jungle",
+    id: 662708,
+    backDrop:
+      "https://m.media-amazon.com/images/M/MV5BNTZlNzc2MmItYWUxYi00NzBlLTg2M2EtNTU3YjZiZjI3Zjc5XkEyXkFqcGdeQXVyMTA3MzQ4MTg0._V1_.jpg",
+  },
 ];
 
 export default data;
