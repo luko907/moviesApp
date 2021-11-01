cce25bce4c98aeffcb99db27d76d54b2a3a284c7
Lucas Koch <luko907@hotmail.com>
2021-10-31 21:53:54 -0300

    add information about movie

--- src/components/MovieInfo/MovieInfo.jsx
+++ src/components/MovieInfo/MovieInfo.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import { StyleRoot } from "radium";
 import styles from "./MovieInfo.module.css";
 import { connect } from "react-redux";
@@ -10,9 +9,9 @@ import "../../App.css";
 
 function MovieInfo(props) {
   const [movieDetails, setMovieDetails] = useState([]);
+  const [info, setInfo] = useState([]);
   const [flag, setFlag] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
-
   const params = useParams();
 
   const baseUrl = "https://image.tmdb.org/t/p/w500";
@@ -34,11 +33,14 @@ function MovieInfo(props) {
   ///////////////////////
 
   useEffect(() => {
-    const url = `process.env.REACT_APP_API_TMDB_BASE_URL/${params.id}?api_key=process.env.REACT_APP_API_TMDB_API_KEY&language=en-US`;
+    const url1 = `process.env.REACT_APP_API_TMDB_BASE_URL/${params.id}?api_key=process.env.REACT_APP_API_TMDB_API_KEY&language=en-US`;
+    const url2 = `process.env.REACT_APP_IMBD_MOVIEDETAILS=${movieDetails.imdb_id}`;
     const fetchData = async () => {
       try {
-        const resp = await axios.get(url);
-        setMovieDetails(resp.data);
+        const resp1 = await axios.get(url1);
+        const resp2 = await axios.get(url2);
+        setMovieDetails(resp1.data);
+        setInfo(resp2.data);
       } catch (error) {
         console.log("error", error);
       }
@@ -47,7 +49,6 @@ function MovieInfo(props) {
       for (let i = 0; i < data.length; i++) {
         if (data[i].id === movieDetails.id) {
           setFlag(data[i].backDrop);
-
           break;
         } else {
           setFlag(baseUrl + movieDetails.poster_path);
@@ -64,6 +65,7 @@ function MovieInfo(props) {
     movieDetails.length,
     movieDetails.id,
     movieDetails.poster_path,
+    movieDetails.imdb_id,
   ]);
 
   return (
@@ -83,31 +85,29 @@ function MovieInfo(props) {
               </div>
               <div className={styles.description_container}>
                 <div className={styles.title}>
-                  <span>Dune</span>
+                  <span>{movieDetails.title}</span>
                 </div>
                 <div className={styles.rates_container}>
-                  <span>13</span>
-                  <span>83%</span>
-                  <span>91%</span>
-                  <span>IMDb 8.3</span>
+                  <div className={styles.ageRated}>
+                    <span>{info.Rated}</span>
+                  </div>
+                  <div className={styles.imbDRate}>
+                    <span>IMDb {info.Ratings && info.Ratings[0].Value}</span>
+                  </div>
+                  <div className={styles.rating}>
+                    <span>{info.Ratings && info.Ratings[1].Value}</span>
+                  </div>
                 </div>
                 <div className={styles.year_time_genre_container}>
-                  <span>2021</span>
-                  <span>2 hr 35 min</span>
-                  <span>Sci-Fi, Action, Adventure, Drama</span>
+                  <span>{info.Year}</span>
+                  <span>{info.Runtime}</span>
+                  <span>
+                    {movieDetails.genres &&
+                      movieDetails.genres.map((gen) => gen.name).join(" ")}
+                  </span>
                 </div>
-
                 <div className={styles.plot}>
-                  <p>
-                    Paul Atreides, a brilliant and gifted young man born into a
-                    great destiny beyond his understanding, must travel to the
-                    most dangerous planet in the universe to ensure the future
-                    of his family and his people. As malevolent forces explode
-                    into conflict over the planet's exclusive supply of the most
-                    precious resource in existence-a commodity capable of
-                    unlocking humanity's greatest potential-only those who can
-                    conquer their fear will survive.
-                  </p>
+                  <p>{movieDetails.overview}</p>
                 </div>
                 <div className={styles.director_studios_container}>
                   <div className={styles.director_container}>
