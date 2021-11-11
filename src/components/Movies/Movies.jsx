8dcaba8ef8b055e0333b6b60d58a6707575f8841
Lucas Koch <luko907@hotmail.com>
2021-11-11 20:26:28 -0300

    advance on genre filters

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { getMovies } from "../../actions";
 import { connect } from "react-redux";
+import { faFilter } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { NavLink } from "react-router-dom";
 import styles from "./Movies.module.css";
 import "../../App.css";
@@ -8,12 +10,20 @@ import Movie from "../Movie/Movie";
 
 function Movies(props) {
   const [isLoading, setIsLoading] = useState(true);
+  const [selected, setSelected] = useState("Genres");
   useEffect(() => {
     setTimeout(function () {
       setIsLoading(false);
     }, Math.random() * 250);
   }, []);
-
+  function popularHandler() {
+    props.getMovies();
+    setSelected("Genres");
+  }
+  const handleChange = (event) => {
+    setSelected(event.target.value);
+    props.getMovies();
+  };
   return (
     <React.Fragment>
       {(props.moviL && props.moviL.length < 1) || props.moviL === undefined
@@ -24,13 +34,26 @@ function Movies(props) {
       ) : (
         <div className={styles.container}>
           <div className={styles.div_suggestion}>
-            <span>Sort By</span>
+            <FontAwesomeIcon className={styles.filter} icon={faFilter} />
+            <span>Filters</span>
           </div>
           <div className={styles.div_top}>
             <ul className={styles.ul_top}>
               <li>
                 <NavLink className={styles.popular_link} to="/">
-                  <button onClick={() => props.getMovies()}>Popular</button>
+                  <button onClick={() => popularHandler()}>Popular</button>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className={styles.popular_link} to="/">
+                  <select value={selected} onChange={handleChange}>
+                    <option value="Genres" disabled>
+                      Genres
+                    </option>
+                    <option value="Action">Action</option>
+                    <option value="Horror">Horror</option>
+                    <option value="Comedy">Comedy</option>
+                  </select>
                 </NavLink>
               </li>
             </ul>
